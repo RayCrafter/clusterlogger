e3c22478cf7c2fc9e7e99ed4fd9c08092d44f3be
David Zuber <zuber.david@gmx.de>
2015-07-28 16:48:53 +0200

    Bump version: 0.1.1 → 0.2.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,8 +33,8 @@ master_doc = 'index'
 project = u'ClusterLogger'
 copyright = u'2015, David Zuber'
 
-version = '0.1.1'
-release = '0.1.1'
+version = '0.2.0'
+release = '0.2.0'
 
 exclude_patterns = ['_build']
 
