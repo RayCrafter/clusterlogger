952309ee5e0ee973e738675bfb08684d01c7368f
David Zuber <zuber.david@gmx.de>
2015-07-28 14:01:31 +0200

    Add gelf tcp handler

---
+++ src/clusterlogger/handler.py
@@ -0,0 +1,62 @@
+import logging
+import json
+import zlib
+import logging.handlers
+
+import graypy
+import graypy.handler
+
+
+class GELFTCPHandler(logging.handlers.SocketHandler):
+    """Graylog Extended Log Format handler
+
+    This handler uses TCP Sockets.
+    """
+
+    def __init__(self, host, port=12201, chunk_size=graypy.WAN_CHUNK,
+                 debugging_fields=True, extra_fields=True, fqdn=False,
+                 localname=None, facility=None):
+        """Initialize a new GELF TCP Handler
+
+        :param host: The host of the graylog server.
+        :type host: :class:`str`
+        :param port: The port of the graylog server (default 12201).
+        :type port: :class:`int`
+        :param chunk_size: Message chunk size. Messages larger than this
+            size will be sent to graylog in multiple chunks. Defaults to
+            ``WAN_CHUNK=1420``.
+        :type chunk_size: :class:`int`
+        :param debugging_fields: Send debug fields if true (the default).
+        :type debugging_fields: :class:`bool`
+        :param extra_fields: Send extra fields on the log record to graylog
+            if true (the default).
+        :type extra_fields: :class:`bool`
+        :param fqdn: Use fully qualified domain name of localhost as source
+            host (socket.getfqdn()).
+        :type fqdn: :class:`str`
+        :param localname: Use specified hostname as source host.
+        :type localname: :class:`str`
+        :param facility: Replace facility with specified value. If specified,
+            record.name will be passed as `logger` parameter.
+        :type facility: :class:`str`
+        """
+        self.debugging_fields = debugging_fields
+        self.extra_fields = extra_fields
+        self.chunk_size = chunk_size
+        self.fqdn = fqdn
+        self.localname = localname
+        self.facility = facility
+        super(GELFTCPHandler, self).__init__(host, port)
+
+    def send(self, s):
+        if len(s) < self.chunk_size:
+            super(GELFTCPHandler, self).send(self, s)
+        else:
+            for chunk in graypy.handler.ChunkedGELF(s, self.chunk_size):
+                super(GELFTCPHandler, self).send(self, chunk)
+
+    def makePickle(self, record):
+        message_dict = graypy.handler.make_message_dict(
+            record, self.debugging_fields, self.extra_fields, self.fqdn,
+            self.localname, self.facility)
+        return zlib.compress(json.dumps(message_dict).encode('utf-8'))
